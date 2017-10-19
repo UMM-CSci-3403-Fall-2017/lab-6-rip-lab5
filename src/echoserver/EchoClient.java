6c34a486e6f504ed33eff2fcb1477be0c3748403
hoff0899 <hoff0899@morris.umn.edu>
2017-10-19 11:30:33 -0500

    Start implementation of EchoClient.java

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,23 @@
 package echoserver;
 
+import java.net.*;
+import java.io.*;
+
 public class EchoClient {
+	public static void main(String [] args){
+		int inChar;
+//		System.out.println("Enter in stuff, type 'Exit' to close.");
+		try {
+			//String hostname = args[0];
+			//Socket socket = new Socket (hostname, 6013); 
+			
+			while((inChar = System.in.read()) != -1) {
+				System.out.println(inChar);
+			}
+			
+		} catch (IOException ioe) {
+			System.out.println("You gave us an error, nerd.");
+			System.out.println(ioe);
+		}
+	}
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,21 @@
 package echoserver;
 
+import java.net.*;
+import java.io.*;
+
 public class EchoServer {
+	public static void main(String [] args) {
+		try {
+			ServerSocket socket = new ServerSocket(6013);
+			
+			while(true) {
+				System.out.println("Get a request!");
+				Socket client = socket.accept();
+				
+				PrintWriter writer = new PrintWriter(client.getOutputStream(), true);
+				
+				writer.println()
+			}
+		}
+	}
 }
